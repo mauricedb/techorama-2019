c365d1f9f62572cd38d13864f7b930d3b27f1510
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-09-17 14:18:48 +0200

    Lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
-import Jokes from "./components/jokes";
+// import Jokes from "./components/jokes";
+import Loading from "./components/loading";
+
+const Jokes = React.lazy(() => import("./components/jokes"));
 
 const App: React.FC = () => {
   const [displayJokes, setDisplayJokes] = React.useState(false);
 
   return (
     <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={displayJokes}
-          onChange={() => setDisplayJokes(!displayJokes)}
-        />
-        Display jokes
-      </label>
-      {displayJokes && <Jokes url="/api/jon-skeet.json" />}
+      <Suspense fallback={<Loading />}>
+        <label>
+          <input
+            type="checkbox"
+            checked={displayJokes}
+            onChange={() => setDisplayJokes(!displayJokes)}
+          />
+          Display jokes
+        </label>
+        {displayJokes && <Jokes url="/api/jon-skeet.json" />}
+      </Suspense>
     </div>
   );
 };
